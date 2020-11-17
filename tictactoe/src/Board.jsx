86113148e8e66f47c48ace3415ca3113f2faacee
limseongkyu <96x60812@naver.com>
2020-11-17 12:02:51 +0900

    [Add] state

--- tictactoe/src/Board.jsx
+++ tictactoe/src/Board.jsx
@@ -2,8 +2,19 @@ import React from "react";
 import Square from "./Square";
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.hadleClick(i)}
+      />
+    );
   }
 
   render() {
--- tictactoe/src/Square.jsx
+++ tictactoe/src/Square.jsx
@@ -1,16 +1,9 @@
 import React from "react";
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
   render() {
     return (
-      <button className="square" onClick={() => this.setState({ value: "X" })}>
+      <button className="square" onClick={() => this.props.onClick()}>
         {this.state.value}
       </button>
     );
