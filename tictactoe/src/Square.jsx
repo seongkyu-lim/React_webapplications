e39a0f73c975047fee842725fd8682b11f790808
limseongkyu <96x60812@naver.com>
2020-11-17 12:15:48 +0900

    [Changed] square component to functional

--- tictactoe/src/Square.jsx
+++ tictactoe/src/Square.jsx
@@ -10,4 +10,12 @@ class Square extends React.Component {
   }
 }
 
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
 export default Square;
