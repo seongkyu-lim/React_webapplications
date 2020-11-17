28315b27872028474a94e1c3e061acf038731f92
limseongkyu <96x60812@naver.com>
2020-11-17 12:27:22 +0900

    [Add] decide winner

--- tictactoe/src/Square.jsx
+++ tictactoe/src/Square.jsx
@@ -1,15 +1,5 @@
 import React from "react";
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
-}
-
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
