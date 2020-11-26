baac2d266cc37d3e9eeea1efa903438dcaf4a4ed
limseongkyu <96x60812@naver.com>
2020-11-26 15:10:39 +0900

    [Add] params

--- react-router-tutorial/src/shared/App.js
+++ react-router-tutorial/src/shared/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import { Home, About } from "../pages";
 
 class App extends Component {
   render() {
-    return <div>Hello React-Router</div>;
+    return (
+      <div>
+        <Route exact path="/" component={Home} />
+        <Switch>
+          <Route path="/about/:name" component={About} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </div>
+    );
   }
 }
 
