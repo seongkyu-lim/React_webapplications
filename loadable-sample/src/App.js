3cbe88b940a5a8909f09c06402ba82cd626568ea
limseongkyu <96x60812@naver.com>
2020-11-27 17:03:29 +0900

    [Add]HoC with router

--- loadable-sample/src/App.js
+++ loadable-sample/src/App.js
@@ -1,23 +1,22 @@
 import React, { Component } from "react";
-import withSplitting from "./withSplitting";
-
-const SplitMe = withSplitting(() => import("./SplitMe"));
+import { Route, Link } from "react-router-dom";
+import { About, Home } from "./pages";
 
 class App extends Component {
-  state = {
-    visible: false,
-  };
-  handleClick = () => {
-    this.setState({
-      visible: true,
-    });
-  };
   render() {
-    const { visible } = this.state;
     return (
       <div>
-        <button onClick={this.handleClick}>Click Me</button>
-        {visible && <SplitMe />}
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+        <hr />
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
       </div>
     );
   }
